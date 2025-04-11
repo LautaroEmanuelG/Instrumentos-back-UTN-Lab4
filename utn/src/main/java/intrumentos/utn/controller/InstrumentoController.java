1bc62d711854578391528b21d514deb8d499821f
LautaroEmanuelG <lgonzalez.sag98@gmail.com>
2025-04-11 10:00:48 -0300

    feat: Agregar nuevos atributos y métodos en la clase Instrumento

--- utn/src/main/java/intrumentos/utn/controller/InstrumentoController.java
+++ utn/src/main/java/intrumentos/utn/controller/InstrumentoController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/instrumentos")
+@CrossOrigin(origins = "http://localhost:5173")
 public class InstrumentoController {
     @Autowired
     private InstrumentoService service;
--- utn/src/main/java/intrumentos/utn/model/Instrumento.java
+++ utn/src/main/java/intrumentos/utn/model/Instrumento.java
@@ -13,13 +13,93 @@ public class Instrumento {
     private String tipo;
     private double precio;
 
+    // Nuevos atributos
+    private String marca;
+    private String modelo;
+    private String imagen;
+    private String costoEnvio;
+    private String cantidadVendida;
+    private String descripcion;
+
     // Getters y setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
-    public String getTipo() { return tipo; }
-    public void setTipo(String tipo) { this.tipo = tipo; }
-    public double getPrecio() { return precio; }
-    public void setPrecio(double precio) { this.precio = precio; }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    // Nuevos getters y setters
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getCostoEnvio() {
+        return costoEnvio;
+    }
+
+    public void setCostoEnvio(String costoEnvio) {
+        this.costoEnvio = costoEnvio;
+    }
+
+    public String getCantidadVendida() {
+        return cantidadVendida;
+    }
+
+    public void setCantidadVendida(String cantidadVendida) {
+        this.cantidadVendida = cantidadVendida;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
