22ab4963fa7b025ff1ae4d41945c68ae3ec7e91f
LautaroEmanuelG <lgonzalez.sag98@gmail.com>
2025-04-18 15:15:22 -0300

    Feat (Caterogia, Initializer, updates): Actualizar configuración de dependencias, renombrar rutas de API y agregar inicialización de datos

--- utn/src/main/java/intrumentos/utn/model/Categoria.java
+++ utn/src/main/java/intrumentos/utn/model/Categoria.java
@@ -1,29 +1,18 @@
 package intrumentos.utn.model;
 
 import jakarta.persistence.*;
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "categoria")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Categoria {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nombre;
-
-    // Getters y setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
 }
--- utn/src/main/java/intrumentos/utn/model/Instrumento.java
+++ utn/src/main/java/intrumentos/utn/model/Instrumento.java
@@ -1,118 +1,29 @@
-// c:\Users\Lautaro\Documents\GitHub\Instrumentos-back-UTN-Lab4\src\main\java\com\example\instrumentos\model\Instrumento.java
 package intrumentos.utn.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "instrumento")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Instrumento {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String nombre;
-    private String tipo;
-    private double precio;
-
-    // Relación con Categoria
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "id_categoria")
-    private Categoria categoria;
-
-    // Nuevos atributos
+    private String instrumento;
     private String marca;
     private String modelo;
     private String imagen;
+    private double precio;
     private String costoEnvio;
-    private String cantidadVendida;
+    private Integer cantidadVendida;
     private String descripcion;
 
-    // Getters y setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
-    public double getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(double precio) {
-        this.precio = precio;
-    }
-
-    // Nuevos getters y setters
-    public String getMarca() {
-        return marca;
-    }
-
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
-
-    public String getModelo() {
-        return modelo;
-    }
-
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
-    }
-
-    public String getImagen() {
-        return imagen;
-    }
-
-    public void setImagen(String imagen) {
-        this.imagen = imagen;
-    }
-
-    public String getCostoEnvio() {
-        return costoEnvio;
-    }
-
-    public void setCostoEnvio(String costoEnvio) {
-        this.costoEnvio = costoEnvio;
-    }
-
-    public String getCantidadVendida() {
-        return cantidadVendida;
-    }
-
-    public void setCantidadVendida(String cantidadVendida) {
-        this.cantidadVendida = cantidadVendida;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public Categoria getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(Categoria categoria) {
-        this.categoria = categoria;
-    }
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "idCategoria")
+    private Categoria categoria;
 }
